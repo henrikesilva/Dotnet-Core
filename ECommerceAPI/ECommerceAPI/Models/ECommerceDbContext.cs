5088f57da875be1499b558a796ec37b73a0d664c
henrikesilva <51914997+henrikesilva@users.noreply.github.com>
2020-01-22 14:06:17 -0300

    Inicio .Net Core

---
+++ ECommerceAPI/ECommerceAPI/Models/ECommerceDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Models
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
