ec4030040071a72d03d08dce44cb880ea0da6cab
henrikesilva <51914997+henrikesilva@users.noreply.github.com>
2020-01-22 14:16:40 -0300

    V0.1.0

--- ECommerceAPI/ECommerceAPI/Models/Produto.cs
+++ ECommerceAPI/ECommerceAPI/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace ECommerceAPI.Models
     public class Produto
     {
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
-        public decimal Preco { get; set; }
+        [Required]
+        public decimal? Preco { get; set; }
+        [Required]
         public string Descricao { get; set; }
         public bool Ativo { get; set; }
         public DateTime DataModificacao { get; set; }
